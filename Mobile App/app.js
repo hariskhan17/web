020fe5742063065a1436dfde0191fb494a96ac48
M.HarisKhan <126700535+hariskhan17@users.noreply.github.com>
2023-12-13 19:14:22 +0500

    Add files via upload

---
+++ Mobile App/app.js
@@ -0,0 +1,121 @@
+// axios.get('https://api.escuelajs.co/api/v1/products')
+
+// .then((res)=>{
+// console.log(res);
+// }
+// )
+
+
+
+
+
+
+// const phones = [
+//     {
+//         brand: 'Samsung',
+//         model: 'S20',
+//         ram: 8,
+//         rom: 256,
+//         camera: '20 megapixel',
+//         price: 15000
+//     },
+//     {
+//         brand: 'Xiomi',
+//         model: 'note10',
+//         ram: 4,
+//         rom: 64,
+//         camera: '10 megapixel',
+//         price: 15000
+//     },
+//     {
+//         brand: 'Infinix',
+//         model: 'z10',
+//         ram: 2,
+//         rom: 16,
+//         camera: '5 megapixel',
+//         price: 15000
+//     },
+//     {
+//         brand: 'Tecno',
+//         model: 'spark10',
+//         ram: 12,
+//         rom: 512,
+//         camera: '25 megapixel',
+//         price: 15000
+//     },
+//     {
+//         brand: 'Iphone',
+//         model: '14',
+//         ram: 4,
+//         rom: 1024,
+//         camera: '30 megapixel',
+//         price: 15000
+//     },
+//     {
+//         brand: 'Oppo',
+//         model: 'F11',
+//         ram: 8,
+//         rom: 256,
+//         camera: '20 megapixel',
+//         price: 15000
+//     },
+//     {
+//         brand: 'Vivo',
+//         model: 'y20',
+//         ram: 4,
+//         rom: 64,
+//         camera: '8 megapixel',
+//         price: 15000
+//     },
+//     {
+//         brand: 'Abdullah',
+//         model: 's50',
+//         ram: 50,
+//         rom: 1024,
+//         camera: '60 megapixel',
+//         price: 300000
+//     },
+
+// ]
+// let array = [];
+
+// const div = document.querySelector('.phones')
+// for (let i = 0; i < phones.length; i++) {
+//     div.innerHTML += `${phones[i].brand} <br>
+//     ${phones[i].brand}<br>
+//     ${phones[i].model}<br>
+//     ${phones[i].ram}<br>
+//     ${phones[i].rom}<br>
+//     ${phones[i].camera}<br>
+//     ${phones[i].price}<br>
+//     <br>
+//     <button onclick="renarr(${i})" > submit </button> <br>
+//    <br> `}
+// function renarr(index) {jkmk
+//     array.push(phones[index])
+//     console.log(array);
+
+//     Swal.fire({
+//         title: "Thanks For Submit!",
+//         text: "That thing is still around?",
+//         icon: "done"
+//     });
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
